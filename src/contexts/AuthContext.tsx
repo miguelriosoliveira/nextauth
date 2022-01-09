adf5f2120e58a1982803bea00e0acb8b3d9aded5
Miguel Rios <miguelriosoliveira@gmail.com>
2022-01-09 17:09:37 -0300

    feature: Axios client and React-Toastify

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,4 +1,8 @@
+import { AxiosError } from 'axios';
 import { createContext, ReactNode, useMemo } from 'react';
+import { toast } from 'react-toastify';
+
+import { api } from '../services/api';
 
 // ========================================== AuthContext ==========================================
 
@@ -20,12 +24,25 @@ interface AuthProviderProps {
 	children: ReactNode;
 }
 
+interface AuthenticationResponseError {
+	error: boolean;
+	message: string;
+}
+// const notifyError = (message: string) => toast.error(message, { theme: 'colored' });
+const notifyError = (message: string) => toast.error(message, { theme: 'colored' });
+
 async function signIn({ email, password }: SignInCredentials) {
-	console.log({ email, password });
+	try {
+		const { data } = await api.post('sessions', { email, password });
+	} catch (error) {
+		const axiosError = error as AxiosError<AuthenticationResponseError>;
+		notifyError(axiosError.response?.data.message as string);
+	}
 }
 
 export function AuthProvider({ children }: AuthProviderProps) {
 	const isAuthenticated = false;
+
 	const providerValues = useMemo(() => ({ signIn, isAuthenticated }), [isAuthenticated]);
 
 	return <AuthContext.Provider value={providerValues}>{children}</AuthContext.Provider>;
