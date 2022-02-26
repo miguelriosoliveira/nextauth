d6e1e089248fed58c5f920731d9803de41d487f4
Miguel Rios <miguelriosoliveira@gmail.com>
2022-02-26 19:22:51 -0300

    wip: withSsrAuth wrapper

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,7 @@
 import { NextPage } from 'next';
 
 import { useAuth } from '../hooks';
+import { withSsrAuth } from '../utils/withSsrAuth';
 
 const Dashboard: NextPage = () => {
 	const { user } = useAuth();
@@ -9,3 +10,7 @@ const Dashboard: NextPage = () => {
 };
 
 export default Dashboard;
+
+export const getServerSideProps = withSsrAuth(async () => {
+	return { props: {} };
+});
